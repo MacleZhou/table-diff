1a1c03ed7d9d8948d1d7a24de22cea788b1bcc36
dmn1k <dominik.schlosser@gmail.com>
2018-07-08 21:52:55 +0200

    Refactoring

--- src/main/java/com/github/dmn1k/table/diff/ColumnComparisonStrategies.java
+++ src/main/java/com/github/dmn1k/table/diff/ColumnComparisonStrategies.java
@@ -9,16 +9,17 @@ public final class ColumnComparisonStrategies {
         // static class
     }
 
-    public static final Function2<TableCell, TableCell, Boolean> IGNORE_ALL_MISSING_COLUMNS =
-            (a, b) -> a.isMissing() || b.isMissing() || Objects.equals(a.getValue(), b.getValue());
-
-    public static final Function2<TableCell, TableCell, Boolean> IGNORE_MISSING_COLUMNS_IN_NEW_TABLE =
-            (a, b) -> a.isMissing() || Objects.equals(a.getValue(), b.getValue());
-
-    public static final Function2<TableCell, TableCell, Boolean> IGNORE_MISSING_COLUMNS_IN_OLD_TABLE =
-            (a, b) -> b.isMissing() || Objects.equals(a.getValue(), b.getValue());
-
-
     public static final Function2<TableCell, TableCell, Boolean> CONSIDER_MISSING_COLUMNS_AS_CHANGE =
             (a, b) -> Objects.equals(a.getValue(), b.getValue());
+
+    public static final Function2<TableCell, TableCell, Boolean> IGNORE_ALL_MISSING_COLUMNS =
+            (a, b) -> a.isMissing() || b.isMissing() || CONSIDER_MISSING_COLUMNS_AS_CHANGE.apply(a, b);
+
+    public static final Function2<TableCell, TableCell, Boolean> IGNORE_MISSING_COLUMNS_IN_NEW_TABLE =
+            (a, b) -> a.isMissing() || CONSIDER_MISSING_COLUMNS_AS_CHANGE.apply(a, b);
+
+    public static final Function2<TableCell, TableCell, Boolean> IGNORE_MISSING_COLUMNS_IN_OLD_TABLE =
+            (a, b) -> b.isMissing() || CONSIDER_MISSING_COLUMNS_AS_CHANGE.apply(a, b);
+
+
 }
